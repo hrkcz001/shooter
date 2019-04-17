9522f507c20374bdd7e48cecd33cefc86b36d606
hrkcz001 <hrkcz001@gmail.com>
2019-04-17 22:26:09 +0300

    0.1.0 (s)

--- Client.java
+++ Client.java
@@ -141,13 +141,106 @@ class Wind extends JFrame{
 
 class MainPane extends JPanel{
 
+	Wind wind;
+	Dimension size;
+	int currentX;
+	int currentY;
+	JPanel startGamePanel;
+
   public MainPane(Wind wind, Dimension size) throws Exception{
 
     super();
     setSize(size);
     setLocation(0, 0);
-    setBackground(Color.GREEN);
+    setBackground(new Color(100, 200, 100));
 		setLayout(null);
+		this.wind = wind;
+		this.size = size;
+
+		createServerTable();
+
+		JButton startButton = new JButton("Start");
+		startButton.setLocation(0, 0);
+		startButton.setSize(100, 50);
+		startButton.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent ae){
+
+				startGamePanel.setLocation(size.width / 6, size.height / 10);
+				startGamePanel.setVisible(true);
+
+			}
+
+		});
+
+		add(startButton);
+
+  }
+
+	public void createServerTable(){
+
+		startGamePanel = new JPanel();
+
+		startGamePanel.setLocation(size.width / 6, size.height / 10);
+		startGamePanel.setSize(size.width * 2 / 3, size.height * 3 / 4 + size.height / 20);
+		startGamePanel.setBackground(new Color(255, 100, 255));
+		startGamePanel.setLayout(null);
+
+		startGamePanel.addMouseListener(new MouseAdapter(){
+
+			public void mousePressed(MouseEvent e) {
+					currentX = e.getX();
+					currentY = e.getY();
+			}
+
+			public void mouseDragged(MouseEvent e) {
+					startGamePanel.setLocation(startGamePanel.getLocation().x + e.getX() - currentX, startGamePanel.getLocation().y + e.getY() - currentY);
+			}
+
+		});
+		startGamePanel.addMouseMotionListener(new MouseMotionAdapter() {
+							public void mouseDragged(MouseEvent e) {
+
+									startGamePanel.setLocation(startGamePanel.getLocation().x + e.getX() - currentX,
+													startGamePanel.getLocation().y + e.getY() - currentY);
+							}
+		});
+
+		JLabel close = new JLabel("close");
+		close.setForeground(new Color(150, 255, 150));
+		close.setLocation(size.width * 2 / 3 - size.width / 40, size.height / 640);
+		close.setSize(size.width / 20, size.height / 40);
+		close.setFont(new Font("Arial", Font.BOLD, size.height / 60));
+		close.addMouseListener(new MouseAdapter(){
+
+			public void mouseClicked(MouseEvent me){
+
+				startGamePanel.setVisible(false);
+
+			}
+
+		});
+		startGamePanel.add(close);
+
+		JLabel reload = new JLabel("reload");
+		reload.setForeground(new Color(150, 255, 150));
+		reload.setLocation(size.width * 2 / 3 - size.width / 15, size.height / 640);
+		reload.setSize(size.width / 20, size.height / 40);
+		reload.setFont(new Font("Arial", Font.BOLD, size.height / 60));
+		reload.addMouseListener(new MouseAdapter(){
+
+			public void mouseClicked(MouseEvent me){
+
+				Point curPoint = startGamePanel.getLocation();
+				startGamePanel.setVisible(false);
+				createServerTable();
+				startGamePanel.setLocation(curPoint);
+				startGamePanel.setVisible(true);
+
+			}
+
+		});
+		startGamePanel.add(reload);
 
 		WaitServerConnecter wsc = new WaitServerConnecter();
 		Boolean connectionRes = wsc.setSocket("localhost",6900);
@@ -156,12 +249,13 @@ class MainPane extends JPanel{
 
 				JTextArea jta = new JTextArea("Player");
 				jta.setSize(size.width * 2 / 3, size.height / 40);
-				jta.setLocation(size.width / 6, size.height / 8);
-				add(jta);
+				jta.setLocation(0, size.height / 40);
+				jta.setBackground(new Color(150, 255, 150));
+				jta.setForeground(new Color(255, 100, 255));
+				startGamePanel.add(jta);
 
 			  String answ = wsc.getServerList();
 				String[] server = answ.split(":");
-				String[] tableNames = {"Name", "Port", "Players"};
 				String[][] tableData = new String[server.length - 1][3];
 
 				for(int i = 1; i < server.length; i++){
@@ -200,21 +294,18 @@ class MainPane extends JPanel{
 				};
 
 				JTable table = new JTable(tm);
+				table.getColumnModel().setColumnSelectionAllowed(false);
 
-				int height = (size.height * 3 / 4) / (server.length - 1);
-				if(height < 20){
+				int height = size.height / 45;
 
-					height = 20;
-
-				}
-
-				JScrollPane  js = new JScrollPane(table);
+				JPanel serverPanel = new JPanel();
+				JScrollPane js = new JScrollPane(serverPanel);
 
 				for(int i = 0; i < tableData.length; i++){
 
 					JButton jb = new JButton("Connect");
-					jb.setLocation(size.width * 5 / 6 - 1, height * i + (height / 4 + size.height / 600)*(tableData.length));
-					jb.setSize(size.width / 9, height);
+					jb.setLocation(size.width * 2 / 3 - size.width / 9, height * i);
+					jb.setSize(size.width / 9 - size.width / 150, height);
 					final int tmp = i;
 					jb.addActionListener(new ActionListener(){
 						public void actionPerformed(ActionEvent ae){
@@ -230,36 +321,49 @@ class MainPane extends JPanel{
 						}
 					});
 
-					add(jb);
+					serverPanel.add(jb);
 
 				}
 
 				table.setRowHeight(height);
-				table.setLocation(size.width / 6, size.height / 6);
-				table.setSize(size.width * 2 / 3, size.height * 3 / 4);
-				js.setLocation(size.width / 6, size.height / 6);
-				js.setSize(size.width * 2 / 3, size.height * 4 / 5);
+				table.setLocation(0, 0);
+				table.setSize(size.width * 2 / 3 - size.width / 9, height * tableData.length);
+				serverPanel.add(table);
+				serverPanel.setLayout(null);
+				serverPanel.setPreferredSize(new Dimension(size.width * 2 / 3, height * tableData.length));
+				js.setLocation(0, size.height / 20);
+				js.setSize(size.width * 2 / 3, size.height * 3 / 4);
+				js.getVerticalScrollBar().setPreferredSize(new Dimension(size.width / 150, 0));
 
-				table.getColumnModel().getColumn(0).setPreferredWidth(size.width / 3);
-				table.getColumnModel().getColumn(1).setPreferredWidth(size.width / 6);
-				table.getColumnModel().getColumn(2).setPreferredWidth(size.width / 6);
+				js.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				js.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-				add(js);
+				table.getColumnModel().getColumn(0).setPreferredWidth(size.width / 3 - size.width / 27);
+				table.getColumnModel().getColumn(1).setPreferredWidth(size.width / 6 - size.width / 27);
+				table.getColumnModel().getColumn(2).setPreferredWidth(size.width / 6 - size.width / 27);
+
+				startGamePanel.add(js);
+
+				startGamePanel.setVisible(false);
+
+				add(startGamePanel);
 
     }
     else{
 
-     JLabel refused = new JLabel("Не установлено соединение с сервером. Проверьте подключение к интернету и повторите попытку.");
+     JLabel refused = new JLabel("Не установлено соединение с сервером");
      refused.setFont(new Font("Arial", Font.BOLD, size.height / 50));
      refused.setForeground(Color.RED);
-     refused.setLocation(size.width / 5, size.height / 2);
+     refused.setLocation(size.width / 5, size.height * 3 / 8 + size.height / 40);
 		 refused.setSize(size.width, size.height / 50);
-     add(refused);
+     startGamePanel.add(refused);
+
+		 startGamePanel.setVisible(false);
+		 add(startGamePanel);
 
     }
 
-
-  }
+	}
 
 }
 
