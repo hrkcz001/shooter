18b7cdf1bbb2a72d551d546c8e823e2b257fc9b7
IIE4EHbKAKOKC <49307215+IIE4EHbKAKOKC@users.noreply.github.com>
2019-04-26 11:14:35 +0300

    Add files via upload
    вроде бы все должно работать

---
+++ Client.java
@@ -0,0 +1,500 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.text.DecimalFormat;
+
+class Client {
+
+	public static void main (String [] args) throws Exception{
+
+    Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+
+    Wind frame = new Wind(size);
+
+  }
+
+}
+
+class WaitServerConnecter{
+
+	DataInputStream dis;
+  DataOutputStream dos;
+	Socket s;
+
+	public Boolean setSocket(String ip, int port){
+
+		try{
+
+			s = new Socket (ip,port);
+			dis = new DataInputStream(s.getInputStream());
+			dos = new DataOutputStream(s.getOutputStream());
+
+			return(true);
+
+		}
+		catch(Exception e){
+
+			System.out.println(e);
+			return(false);
+
+		}
+
+	}
+
+	public String getServerList(){
+
+		String res;
+
+	  try{
+
+			res = dis.readUTF();
+			s.close();
+			return(res);
+
+		}
+		catch(Exception e) {
+
+			System.out.println(e);
+			return("false");
+
+		}
+
+	}
+
+}
+
+class GameServerConnection {
+
+	DataInputStream dis;
+  DataOutputStream dos;
+  String[] map;
+	String username = "PLAYER";
+
+	public GameServerConnection(String ip, int port) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		Socket s = new Socket (ip,port);
+		dis = new DataInputStream(s.getInputStream());
+		dos = new DataOutputStream(s.getOutputStream());
+
+	}
+
+	public void enterServer(String username) throws Exception {
+
+		this.username = username;
+		dos.writeUTF(username);
+		String answ = dis.readUTF();
+		System.out.println(answ);
+
+	}
+
+	public void downloadMap() throws Exception {
+
+			int length = Integer.parseInt(dis.readUTF());
+			System.out.println(length);
+			map = new String[length];
+			for (int i = 0; i < length; i++) {
+
+				map[i] = dis.readUTF();
+
+			}
+
+			dos.writeUTF(dis.readUTF());
+
+ }
+
+}
+
+class Screen extends JComponent{
+
+	char[][] map;
+	Dimension size;
+	DataInputStream dis;
+
+	public Screen(GameServerConnection gsc, Dimension size){
+
+		dis = gsc.dis;
+		String[] stringMap = gsc.map;
+		map = new char[stringMap.length][];
+		for(int i = 0; i < stringMap.length; i++){
+			map[i] = stringMap[i].toCharArray();
+		}
+		this.size = size;
+
+		javax.swing.Timer timer = new javax.swing.Timer(5, new ActionListener(){
+
+			public void actionPerformed(ActionEvent ae){
+
+				repaint();
+
+			}
+
+		});
+
+		timer.start();
+
+	}
+
+  public void paintComponent(Graphics gr){
+
+		super.paintComponents(gr);
+		Graphics2D g =(Graphics2D)(gr);
+
+		DecimalFormat df = new DecimalFormat("#.00");
+
+		try{
+
+			g.setPaint(Color.BLACK);
+			g.fillRect(0, 0, 1600, 900);
+
+			System.out.println(dis.readUTF());
+			System.out.println(dis.readUTF());
+			System.out.println(dis.readUTF());
+
+			String[] bulletString = dis.readUTF().split("&")[0].split(":");
+
+			g.setPaint(Color.RED);
+
+			if(!bulletString[0].isEmpty())
+			for(int i = 0; i < bulletString.length; i++){
+				
+				System.out.print(bulletString[i].split("/")[0] + ":" + bulletString[i].split("/")[1]);
+
+				g.fillRect((int)df.parse(bulletString[i].split("/")[0]).doubleValue(), (int)df.parse(bulletString[i].split("/")[1]).doubleValue(), 10, 10);
+
+			}
+
+			//repaint();
+
+		}catch(Exception e){e.printStackTrace();}
+
+		/*for(int i = 0; i < map.length; i++)
+			for(int j = 0; j < map[i].length; j++){
+				switch(map[i][j]){
+					case 'f': g.setPaint(Color.GREEN);
+					          break;
+					case 'b': g.setPaint(Color.BLACK);
+										break;
+				}
+				//g.fillRect(j*10 + size.width / 2 - map[0].length * 5, i*10 + size.height / 2 - map.length * 5, 10, 10);
+			}*/
+
+  }
+
+}
+
+class Wind extends JFrame{
+
+	final Dimension size;
+	Screen screen;
+
+  public Wind(Dimension size) throws Exception{
+
+    super("Shooter");
+		this.size = size;
+    setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setUndecorated(true);
+    setResizable(false);
+
+    MainPane main = new MainPane(this, size);
+
+		main.addKeyListener(new KeyAdapter(){
+
+			public void keyRealesed(KeyEvent ke){
+
+				System.out.println(ke.getKeyCode());
+
+				if((screen != null) && ((ke.getKeyCode() == 77) || (ke.getKeyCode() == 109))){
+
+					screen.setVisible(false);
+
+				}
+
+			}
+
+		});
+
+    add(main);
+    setVisible(true);
+    main.setVisible(true);
+
+  }
+
+}
+
+class MainPane extends JLayeredPane{
+
+	Wind wind;
+	Dimension size;
+	int currentX;
+	int currentY;
+	JPanel startGamePanel;
+
+  public MainPane(Wind wind, Dimension size) throws Exception{
+
+    super();
+    setSize(size);
+    setLocation(0, 0);
+		setOpaque(true);
+    setBackground(new Color(100, 200, 100));
+		setLayout(null);
+		this.wind = wind;
+		this.size = size;
+		this.wind = wind;
+
+		createServerTable();
+
+		JLabel startButton = new JLabel("Find Server");
+		startButton.setLocation(size.width / 80, size.height / 2);
+		startButton.setFont(new Font("Arial", Font.BOLD, size.height / 36));
+		startButton.setForeground(new Color(200, 50, 200));
+		startButton.setSize(size.height / 5, size.height / 36);
+		startButton.addMouseListener(new MouseAdapter(){
+
+			public void mouseClicked(MouseEvent ae){
+
+				startGamePanel.setLocation(size.width / 6, size.height / 10);
+				startGamePanel.setVisible(true);
+
+			}
+
+		});
+		add(startButton, 2);
+
+
+
+  }
+
+	public void createServerTable(){
+
+		startGamePanel = new JPanel();
+
+		startGamePanel.setLocation(size.width / 6, size.height / 10);
+		startGamePanel.setSize(size.width * 2 / 3, size.height * 3 / 4 + size.height / 20);
+		startGamePanel.setBackground(new Color(255, 100, 255));
+		startGamePanel.setLayout(null);
+
+		startGamePanel.addMouseListener(new MouseAdapter(){
+
+			public void mousePressed(MouseEvent e) {
+					currentX = e.getX();
+					currentY = e.getY();
+			}
+
+			public void mouseDragged(MouseEvent e) {
+					startGamePanel.setLocation(startGamePanel.getLocation().x + e.getX() - currentX, startGamePanel.getLocation().y + e.getY() - currentY);
+			}
+
+		});
+		startGamePanel.addMouseMotionListener(new MouseMotionAdapter() {
+							public void mouseDragged(MouseEvent e) {
+
+									startGamePanel.setLocation(startGamePanel.getLocation().x + e.getX() - currentX,
+													startGamePanel.getLocation().y + e.getY() - currentY);
+							}
+		});
+
+		JLabel close = new JLabel("close");
+		close.setForeground(new Color(150, 255, 150));
+		close.setLocation(size.width * 2 / 3 - size.width / 40, size.height / 640);
+		close.setSize(size.height / 10, size.height / 40);
+		close.setFont(new Font("Arial", Font.BOLD, size.height / 60));
+		close.addMouseListener(new MouseAdapter(){
+
+			public void mouseClicked(MouseEvent me){
+
+				startGamePanel.setVisible(false);
+
+			}
+
+		});
+		startGamePanel.add(close);
+
+		JLabel reload = new JLabel("reload");
+		reload.setForeground(new Color(150, 255, 150));
+		reload.setLocation(size.width * 2 / 3 - size.width / 15, size.height / 640);
+		reload.setSize(size.height / 10, size.height / 40);
+		reload.setFont(new Font("Arial", Font.BOLD, size.height / 60));
+		reload.addMouseListener(new MouseAdapter(){
+
+			public void mouseClicked(MouseEvent me){
+
+				Point curPoint = startGamePanel.getLocation();
+				startGamePanel.setVisible(false);
+				createServerTable();
+				startGamePanel.setLocation(curPoint);
+				startGamePanel.setVisible(true);
+
+			}
+
+		});
+		startGamePanel.add(reload);
+
+		WaitServerConnecter wsc = new WaitServerConnecter();
+		Boolean connectionRes = wsc.setSocket("localhost",6900);
+
+		if(connectionRes){
+
+				JTextArea jta = new JTextArea("Player");
+				jta.setSize(size.width * 2 / 3, size.height / 40);
+				jta.setLocation(0, size.height / 40);
+				jta.setBackground(new Color(150, 255, 150));
+				jta.setForeground(new Color(255, 100, 255));
+				startGamePanel.add(jta);
+
+			  String answ = wsc.getServerList();
+				String[] server = answ.split(":");
+				String[][] tableData = new String[server.length - 1][3];
+
+				for(int i = 1; i < server.length; i++){
+
+					tableData[i - 1] = server[i].split("/");
+
+				}
+
+				TableModel tm = new DefaultTableModel(){
+  				public boolean isCellEditable(int rowIndex, int columnIndex){
+      			return(false);
+  				}
+					public Class<?> getColumnClass(int columnIndex) {
+    				return(String.class);
+					}
+					public int getColumnCount() {
+    				return(3);
+					}
+					public String getColumnName(int columnIndex) {
+    				switch (columnIndex) {
+        			case 0:
+            			return "Name";
+        			case 1:
+            			return "Port";
+        			case 2:
+            			return "Players";
+        		}
+    				return "";
+					}
+					public int getRowCount() {
+    				return(tableData.length);
+					}
+					public Object getValueAt(int rowIndex, int columnIndex){
+						return(tableData[rowIndex][columnIndex]);
+					}
+				};
+
+				JTable table = new JTable(tm);
+				table.getColumnModel().setColumnSelectionAllowed(false);
+
+				int height = size.height / 45;
+
+				JPanel serverPanel = new JPanel();
+				JScrollPane js = new JScrollPane(serverPanel);
+
+				for(int i = 0; i < tableData.length; i++){
+
+					JButton jb = new JButton("Connect");
+					jb.setLocation(size.width * 2 / 3 - size.width / 9, height * i);
+					jb.setSize(size.width / 9 - size.width / 150, height);
+					final int tmp = i;
+					jb.addActionListener(new ActionListener(){
+						public void actionPerformed(ActionEvent ae){
+
+							try{
+								setVisible(false);
+								new ServerConnectionThread(Integer.parseInt(tableData[tmp][1]), jta.getText(), wind);
+							}
+							catch(Exception e){
+								System.out.print(e);
+							}
+
+						}
+					});
+
+					serverPanel.add(jb);
+
+				}
+
+				table.setRowHeight(height);
+				table.setLocation(0, 0);
+				table.setSize(size.width * 2 / 3 - size.width / 9, height * tableData.length);
+				serverPanel.add(table);
+				serverPanel.setLayout(null);
+				serverPanel.setPreferredSize(new Dimension(size.width * 2 / 3, height * tableData.length));
+				js.setLocation(0, size.height / 20);
+				js.setSize(size.width * 2 / 3, size.height * 3 / 4);
+				js.getVerticalScrollBar().setPreferredSize(new Dimension(size.width / 150, 0));
+
+				js.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				js.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+				table.getColumnModel().getColumn(0).setPreferredWidth(size.width / 3 - size.width / 27);
+				table.getColumnModel().getColumn(1).setPreferredWidth(size.width / 6 - size.width / 27);
+				table.getColumnModel().getColumn(2).setPreferredWidth(size.width / 6 - size.width / 27);
+
+				startGamePanel.add(js);
+
+				startGamePanel.setVisible(false);
+
+				add(startGamePanel, 1);
+
+    }
+    else{
+
+     JLabel refused = new JLabel("Не установлено соединение с сервером");
+     refused.setFont(new Font("Arial", Font.BOLD, size.height / 50));
+     refused.setForeground(Color.RED);
+     refused.setLocation(size.width / 5, size.height * 3 / 8 + size.height / 40);
+		 refused.setSize(size.width, size.height / 50);
+     startGamePanel.add(refused);
+
+		 startGamePanel.setVisible(false);
+		 add(startGamePanel, 1);
+
+    }
+
+	}
+
+}
+
+class ServerConnectionThread extends Thread{
+
+	int port;
+	String username;
+	Wind wind;
+
+	public ServerConnectionThread(int port, String username, Wind wind){
+
+		this.port = port;
+		this.username = username;
+		this.wind = wind;
+		start();
+
+	}
+
+	public void run(){
+
+		try{
+
+			GameServerConnection gsc = new GameServerConnection("localhost", port);
+			gsc.enterServer(username);
+			gsc.downloadMap();
+
+			wind.screen = new Screen(gsc , wind.size);
+
+			wind.add(wind.screen);
+			wind.setVisible(true);
+
+		}
+		catch(Exception e){
+
+			System.out.print(e);
+
+		}
+
+	}
+
+}
---
+++ Server.java
@@ -0,0 +1,576 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
+/*import java.nio.*;*/
+
+
+class Server {
+	public static void main(String[]args){
+		new ServerInfo("D:/shooter/serverinfo.txt");
+		//ServerInfo.printAllData();
+		System.out.println(ServerInfo.getData(3));
+		new MainServer();
+	}
+}
+
+class VirtualServer extends Thread{
+	private ServerSocket ss;
+	ArrayList <GameClientThread> clients;
+	int port;
+	int id;
+	public VirtualServer (int id) {
+		this.id = id;
+		clients = new ArrayList<GameClientThread>();
+		//количество игроков на сервере
+		try {
+			port = 6900 + id + 1;
+			ss = new ServerSocket(port);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	public void run () {
+		System.out.println(port);
+		try {
+		while (clients.size() < Integer.parseInt(ServerInfo.getData(2)))
+		 clients.add(new GameClientThread (ss.accept()));
+		 new Game(clients).beforeGame();
+	 }
+	 catch (Exception e) {
+	 	System.out.println(e);
+	 }
+	}
+}
+
+class GameClientThread extends Thread {
+	DataInputStream dis;
+	DataOutputStream dos;
+	OutputStream os;
+	String clientNickname;
+	Socket clientSocket;
+	public GameClientThread (Socket clientSocket) {
+		try {
+			dis = new DataInputStream(clientSocket.getInputStream());
+			dos = new DataOutputStream(clientSocket.getOutputStream());
+			os = clientSocket.getOutputStream();
+			this.clientSocket = clientSocket;
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		start();
+	}
+	public void run () {
+		System.out.println("GameClientThread started");
+		clientNickname = readClientNickname();
+	}
+	public String readClientNickname () {
+		try {
+			String name = dis.readUTF();
+			System.out.println("Nickname " + name);
+			dos.writeUTF("true");
+			return name;
+		}
+		catch (Exception e){
+			System.out.println(e);
+			return null;
+		}
+	}
+	public synchronized void sendString (String s) {
+		try {
+			dos.writeUTF(s);
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}
+	}
+	public synchronized String readString () {
+		try {
+			return(dis.readUTF());
+		}
+		catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+}
+
+class WaitServer extends Thread{
+	private VirtualServer [] servers;
+	public WaitServer (VirtualServer [] servers) {
+		this.servers = servers;
+		System.out.println("WaitServer created");
+	}
+	public void run () {
+		try {
+			ServerSocket ss = new ServerSocket(6900);
+			System.out.println("WaitServer started");
+			while (true)
+			 new ConnectThread (ss.accept(), servers).start();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
+class ConnectThread extends Thread{
+	Socket clientSocket;
+	VirtualServer[] servers;
+	String username;
+	public ConnectThread (Socket s, VirtualServer[] servers) {
+		this.clientSocket = s;
+		this.servers = servers;
+		System.out.println("new ConnectThread");
+	}
+	public void run () {
+		try {
+		DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+		DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+		String answer = "";
+		answer += servers.length;
+		String name = "name";
+		//:port/name/col:
+		for (int i = 0; i < servers.length; i++) answer+=":"+name+"/"+servers[i].port+"/" +"0";
+    dos.writeUTF(answer);
+		System.out.println(answer);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+}
+
+class MainServer {
+	private VirtualServer [] servers;
+	public MainServer () {
+		super();
+		startServer();
+		servers = createVirtualServers(Integer.parseInt(ServerInfo.getData(1)));
+		runServers();
+		new WaitServer(servers).start();
+	}
+	public void startServer () {
+
+	}
+	public void runServers () {
+		for (int i =0; i<servers.length; i++) servers[i].start();
+	}
+	public VirtualServer[] createVirtualServers (int count) {
+		VirtualServer [] servers = new VirtualServer [count];
+		for (int i =0; i<servers.length; i++) servers[i] = new VirtualServer(i);
+		return servers;
+	}
+}
+
+class Game extends Thread {
+	ArrayList <GameClientThread> clients;
+	ArrayList <Gamer> gamers;
+	ArrayList <Barrier> barriers;
+	ArrayList <Camera> cameras;
+	BulletThread bt;
+	String[]map;
+	public Game (ArrayList <GameClientThread> clients) {
+		this.clients = clients;
+	}
+	public void setBarriers() {
+		barriers = new ArrayList<Barrier>();
+	}
+	public void sendMap (GameClientThread gct) {
+		try {
+			System.out.println("Sending the Map");
+			//
+			gct.sendString(Integer.toString(map.length));
+			for (int i = 0;i<map.length; i++) {
+				gct.sendString(map[i]);
+			}
+			//
+			System.out.println("Sending of the Map is done");
+		}
+		catch (Exception e) {
+			System.out.println("Sending of the Map failed");
+			System.out.println(e);
+		}
+	}
+	public void sendMapToAll() {
+		for (int i = 0;i<gamers.size();i++) {
+			sendMap(gamers.get(i).clientThread);
+		}
+	}
+	public String[] readMap () {
+		try {
+	  System.out.println(ServerInfo.getData(3));
+
+		String s = "D:/shooter/maps/map2.txt";
+		if (ServerInfo.getData(3).equals(s)) System.out.println("SOSAMBA");
+		File file = new File(s);
+
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		String str = new String(data, "UTF-8");
+		String[] map = str.split(System.lineSeparator());
+		return map;
+		}
+		catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	public void beforeGame () {
+		map = readMap();
+		System.out.println("Game is running");
+		setBarriers();
+		createPlayers();
+		generatePlayersPositions();
+		sendMapToAll();
+		sendTestString();
+		bt = new BulletThread(gamers, barriers);
+		barriers = new ArrayList <Barrier>();
+		cameras = new ArrayList<Camera>();
+		cameras.add(new Camera(0,0,1600,900,bt,gamers));
+		cameras.get(0).addPlayer(0);
+		//cameras.get(0).addPlayer(1);
+		bt.addDefaultBullet(1.2131231212,2.1323242433454535434663,new DoublePosition(120,101),"red");
+		bt.addDefaultBullet(1,3,new DoublePosition(10,15),"red");
+		//in the end
+		start();
+	}
+	public void sendTestString(){
+		String s;
+		long t1;
+		long t2;
+		long dt;
+		for (int i = 0;i<gamers.size();i++) {
+			t1 = new Date().getTime();
+			gamers.get(i).clientThread.sendString("test");
+			s = gamers.get(i).clientThread.readString();
+			t2 = new Date().getTime();
+			dt = t2 - t1;
+			System.out.println (gamers.get(i).nickname + " ping " + dt);
+		}
+	}
+	public void createPlayers () {
+		gamers = new ArrayList <Gamer>();
+		String team = "none";
+		for (int i = 0;i<clients.size(); i++) {
+			gamers.add(new Gamer(clients.get(i), team));
+		}
+
+	}
+	public boolean allowablePosition(Position p) {
+		char c = map[p.x].charAt(p.y);
+		if (isValid(c)) return true;
+		else return false;
+	}
+	public boolean isValid (char c) {
+		ArrayList<Character> cl = new ArrayList<Character>();
+		cl.add('d');
+		for (int i=0; i<cl.size();i++) if (cl.get(i)==c) return false;
+
+		return true;
+	}
+	public Position randomPosition (int minX,int maxX,int minY,int maxY,int k) {
+		int x = 0;
+		int y = 0;
+		Random r = new Random();
+		x = minX + r.nextInt(maxX - minX);
+		y = minY + r.nextInt(maxY - minY);
+		Position p = new Position(x,y);
+		Position d = new Position(x,y);
+		p.x = (p.x - p.x % k) / k;
+		p.y = (p.y - p.y % k) / k;
+		if (allowablePosition(p)) return d;
+		else return randomPosition(minX,maxX,minY,maxY,k);
+	}
+	public void generatePlayersPositions() {
+		for (int i = 0;i<gamers.size(); i++) {
+			Position p = randomPosition(0,1600,0,900,10);
+			gamers.get(i).setDefaultPosition(p);
+		}
+	}
+	public void cameraUpdate () {
+		 System.out.println(cameras.get(0).forGamer);
+		 //проход по камерам, отправка пользователям координат
+		 for (int i = 0;i<cameras.size();i++)
+		 	for (int j = 0;j<cameras.get(i).gamersId.size();j++)
+				gamers.get(cameras.get(i).gamersId.get(j)).sendString(cameras.get(i).forGamer);
+	}
+	public void run () {
+		try {
+			while (true) {
+				//System.out.println("Update server");
+				cameraUpdate();
+				sleep(10);
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
+class Position {
+	int x;
+	int y;
+	public Position (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public DoublePosition toDoublePosition() {
+		return new DoublePosition (x,y);
+	}
+}
+
+class Gamer {
+	DoublePosition pos;
+	GameClientThread clientThread;
+	String team;
+	String nickname;
+	int health;
+	public Gamer (GameClientThread gct, String team) {
+		this.clientThread = gct;
+		this.team = team;
+		this.nickname = nickname;
+		nickname = clientThread.clientNickname;
+		health = 100;
+	}
+	public void setDefaultPosition (Position p) {
+		pos = p.toDoublePosition();
+		System.out.println("PlayerPosition " + p.x + " " + p.y);
+	}
+	public void damage (int d) {
+		health -= d;
+	}
+	public void sendString (String s) {
+		clientThread.sendString(s);
+	}
+}
+
+class Bullet {
+	double vx;
+	double vy;
+	int radius;
+	DoublePosition p;
+	String team;
+	int damage;
+	ArrayList <Gamer> gamers;
+	ArrayList <Barrier> barriers;
+	public Bullet (double vx,double vy,int radius,int damage,DoublePosition p,ArrayList<Gamer> gamers, String team, ArrayList<Barrier>barriers) {
+		this.vx = vx;
+		this.vy = vy;
+		this.p = p;
+		this.gamers = gamers;
+		this.team = team;
+		this.damage = damage;
+		this.barriers = barriers;
+		radius = 1;
+	}
+	public boolean update () {
+		moove();
+		return makeDamage(checkCollision());
+	}
+	public boolean makeDamage (Gamer g) {
+		if (g==null) return false;
+		//нанесение дамага
+		g.damage(damage);
+		stop();
+		return true;
+	}
+	public void moove () {
+		p.x += vx;
+		p.y += vy;
+	}
+	public void stop () {
+		vx = 0;
+		vy = 0;
+	}
+	public Gamer checkCollision () {
+		for (int i = 0;i<gamers.size();i++) {
+			double s = Math.pow((gamers.get(i).pos.x - p.x),2) + Math.pow((gamers.get(i).pos.y - p.y),2);
+			if ((gamers.get(i).team == team)&&(Math.sqrt(s)<=radius))
+			 return gamers.get(i);
+		}
+		return null;
+	}
+}
+
+class BulletThread extends Thread{
+	ArrayList <Gamer> gamers;
+	ArrayList <Barrier> barriers;
+	ArrayList <Bullet> bullets;
+	int defRadius = 1;
+	int defDamage = 10;
+	public BulletThread (ArrayList <Gamer> gamers, ArrayList <Barrier> barriers) {
+		this.gamers = gamers;
+		this.barriers = barriers;
+		bullets = new ArrayList<Bullet>();
+		//addDefaultBullet(10,10,new Position(10,10),"red");
+		start();
+	}
+	public void run () {
+		try {
+			while (true) {
+				update();
+				sleep(10);
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	public void update () {
+		for (int i = 0;i<bullets.size();i++) {
+			if (bullets.get(i).update()) bullets.remove(i);
+			else if (checkBarrierCollision(bullets.get(i))) bullets.remove(i);
+		}
+	}
+	public boolean checkBarrierCollision (Bullet g){
+		for (int i=0;i<barriers.size();i++) {
+			if (barriers.get(i).checkCollision(g.p.x,g.p.y)) return true;
+		}
+			return false;
+	}
+	public void addDefaultBullet (double vx, double vy,DoublePosition p,String team) {
+		addBullet(vx,vy,defRadius,defDamage,p,gamers,team,barriers);
+	}
+	public void addBullet(double vx,double vy,int radius,int damage,DoublePosition p,ArrayList<Gamer> gamers, String team, ArrayList<Barrier>barriers){
+		bullets.add(new Bullet(vx,vy,radius,damage,p,gamers,team,barriers));
+	}
+}
+
+class Barrier {
+	int xFrom;
+	int yFrom;
+	int width;
+	int height;
+	public Barrier (int x, int y, int w, int h) {
+		xFrom = x;
+		yFrom = y;
+		width = w;
+		height = h;
+	}
+	public boolean checkCollision(double x, double y) {
+		if (xFrom > x) return false;
+		if ((xFrom + width) < x) return false;
+		if (yFrom > y) return false;
+		if ((yFrom + height) < y) return false;
+		return  true;
+	}
+}
+
+class Camera extends Thread{
+	ArrayList<Integer> gamersId;
+	ArrayList<Gamer> gamers;
+	ArrayList<Bullet> bullets;
+	int xFrom;
+	int yFrom;
+	int width;
+	int height;
+	String forGamer;
+	DecimalFormat decimalFormat;
+	public Camera (int x,int y, int sizeX, int sizeY, BulletThread bt, ArrayList <Gamer> gamers) {
+		this.xFrom = x;
+		this.yFrom = y;
+		this.width = sizeX;
+		this.height = sizeY;
+		this.bullets = bt.bullets;
+		this.gamers = gamers;
+		gamersId = new ArrayList<Integer>();
+		decimalFormat = new DecimalFormat("#.00");
+		forGamer = "";
+		System.out.println("Camera created");
+		start();
+	}
+	public void addPlayer(int i) {
+		gamersId.add(i);
+		System.out.println("Gamer add");
+	}
+	public void deletePlayer (int i) {
+		gamersId.remove(i);
+	}
+	public String bulletsToString () {
+		String s = "";
+		DoublePosition p;
+		for (int i = 0;i<bullets.size();i++) {
+			p = bullets.get(i).p;
+			if (inCameraArea(p)) s+= decimalFormat.format(p.x - xFrom) + "/" + decimalFormat.format(p.y - yFrom) + ":";
+		}
+		return s;
+	}
+	public boolean inCameraArea (DoublePosition p) {
+		if (xFrom > p.x) return false;
+		if ((xFrom + width) < p.x) return false;
+		if (yFrom > p.y) return false;
+		if ((yFrom + height) < p.y) return false;
+		return  true;
+	}
+	public String gamersToString () {
+		String s = "";
+		for (int i = 0;i<gamersId.size();i++)
+			s += decimalFormat.format(gamers.get(gamersId.get(i)).pos.x - xFrom) + "/"+ decimalFormat.format(gamers.get(gamersId.get(i)).pos.y - yFrom) + ":";
+		return s;
+	}
+	public String getForGamer() {
+		return (bulletsToString() + "&" + gamersToString());
+	}
+	public void run () {
+		try {
+		while (true) {
+			forGamer = getForGamer();
+			sleep(10);
+		}
+		}
+	catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
+class DoublePosition {
+	double x;
+	double y;
+	public DoublePosition (double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class ServerInfo {
+	public static String[] data;
+	public ServerInfo (String s) {
+		readData(readDataFile(s));
+	}
+	public void readData (String s) {
+		data = new String[5];
+		data = s.split("&");
+	}
+	public String readDataFile (String way) {
+		try {
+		File file = new File(way);
+
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		String str = new String(data, "UTF-8");
+		return str;
+		}
+		catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+
+	}
+	public static String getData (int i) {
+		return  data[i];
+	}
+	public static void printAllData () {
+		for (int i = 0;i<data.length;i++) System.out.print(getData(i) + " ");
+		System.out.println("");
+	}
+}
