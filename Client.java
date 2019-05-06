c50057a021de52910f0241da22b984d91d980f4d
hrkcz001 <hrkcz001@gmail.com>
2019-05-06 19:43:23 +0300

    0.2.0 (n)

--- Client.java
+++ Client.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.*;
+import java.text.DecimalFormat;
 
 class Client {
 
@@ -69,8 +70,8 @@ class WaitServerConnecter{
 class GameServerConnection {
 
 	DataInputStream dis;
-  DataOutputStream dos;
-  String[] map;
+    DataOutputStream dos;
+    String[] map;
 	String username = "PLAYER";
 
 	public GameServerConnection(String ip, int port) throws Exception{
@@ -102,6 +103,8 @@ class GameServerConnection {
 
 			}
 
+			dos.writeUTF(dis.readUTF());
+
  }
 
 }
@@ -109,8 +112,12 @@ class GameServerConnection {
 class Screen extends JComponent{
 
 	char[][] map;
+	Boolean mapOn = false;
 	Dimension size;
 	DataInputStream dis;
+	double kWidth;
+	double kHeight;
+	String[] oldBulletString = {"-100,00/-100,00:"};
 
 	public Screen(GameServerConnection gsc, Dimension size){
 
@@ -121,6 +128,20 @@ class Screen extends JComponent{
 			map[i] = stringMap[i].toCharArray();
 		}
 		this.size = size;
+		kWidth = size.width / 1600.0;
+		kHeight = size.height / 900.0;
+
+		javax.swing.Timer timer = new javax.swing.Timer(5, new ActionListener(){
+
+			public void actionPerformed(ActionEvent ae){
+
+				repaint();
+
+			}
+
+		});
+
+		timer.start();
 
 	}
 
@@ -129,24 +150,67 @@ class Screen extends JComponent{
 		super.paintComponents(gr);
 		Graphics2D g =(Graphics2D)(gr);
 
-		while(true){try{
+		DecimalFormat df = new DecimalFormat("#.00");
 
-			String[] bulletString = dis.readUTF().split(":");
+		if(!mapOn){
+
+		try{
 
 			g.setPaint(Color.BLACK);
-			g.fillRect(0, 0, 1600, 900);
+			g.fillRect(0, 0, size.width, size.height);
 
-			g.setPaint(Color.RED);
+			String dataForPaint = dis.readUTF();
 
-			for(int i = 0; i < bulletString.length; i++){
+			if(!dataForPaint.isEmpty()){
 
-				g.fillRect(Integer.parseInt(bulletString[i].split("/")[0]), Integer.parseInt(bulletString[i].split("/")[1]), 0, 0);
+
+				String[] splitedData = dataForPaint.split("&");
+
+				if(!splitedData[0].isEmpty()){
+
+					g.setPaint(Color.RED);
+
+					for(int i = 0; i < oldBulletString.length; i++){
+
+						g.fillRect((int)(df.parse(oldBulletString[i].split("/")[0]).doubleValue() * kWidth), (int)(df.parse(oldBulletString[i].split("/")[1]).doubleValue() * kHeight), (int)(4 * kWidth), (int)(4 * kHeight));
+
+					}
+
+					String[] bulletString = splitedData[0].split(":");
+
+					oldBulletString = bulletString;
+
+					for(int i = 0; i < bulletString.length; i++){
+
+						g.fillRect((int)(df.parse(bulletString[i].split("/")[0]).doubleValue() * kWidth), (int)(df.parse(bulletString[i].split("/")[1]).doubleValue() * kHeight), (int)(3 * kWidth), (int)(3 * kHeight));
+
+					}
+
+				}
+
+				if(!splitedData[1].isEmpty()){
+
+
+					String[] playersString = splitedData[1].split(":");
+
+					g.setPaint(Color.BLUE);
+
+					for(int i = 0; i < playersString.length; i++){
+
+						g.fillOval((int)(df.parse(playersString[i].split("/")[0]).doubleValue() * kWidth), (int)(df.parse(playersString[i].split("/")[1]).doubleValue() * kHeight), (int)(50 * kWidth), (int)(50 * kHeight));
+
+					}
+
+
+				}
 
 			}
 
-		}catch(Exception e){e.printStackTrace();}}
+		}catch(Exception e){e.printStackTrace();}
+	}
+	else{
 
-		/*for(int i = 0; i < map.length; i++)
+		for(int i = 0; i < map.length; i++)
 			for(int j = 0; j < map[i].length; j++){
 				switch(map[i][j]){
 					case 'f': g.setPaint(Color.GREEN);
@@ -154,8 +218,9 @@ class Screen extends JComponent{
 					case 'b': g.setPaint(Color.BLACK);
 										break;
 				}
-				//g.fillRect(j*10 + size.width / 2 - map[0].length * 5, i*10 + size.height / 2 - map.length * 5, 10, 10);
-			}*/
+				g.fillRect(j*5 + size.width / 2 - map[0].length * 5, i*5 + size.height / 2 - map.length * 5, 5, 5);
+			}
+		}
 
   }
 
@@ -177,15 +242,39 @@ class Wind extends JFrame{
 
     MainPane main = new MainPane(this, size);
 
-		main.addKeyListener(new KeyAdapter(){
+		addKeyListener(new KeyAdapter(){
 
-			public void keyRealesed(KeyEvent ke){
+			public void keyPressed(KeyEvent ke){
 
-				System.out.println(ke.getKeyCode());
+				if(main.started){
 
-				if((screen != null) && ((ke.getKeyCode() == 77) || (ke.getKeyCode() == 109))){
+					switch(ke.getKeyCode()){
 
-					screen.setVisible(false);
+						case 87: main.sct.w = true; break;
+						case 65: main.sct.a = true; break;
+						case 83: main.sct.s = true; break;
+						case 68: main.sct.d = true; break;
+						case 77: if(screen.mapOn){screen.mapOn = false;}
+										 else{screen.mapOn = true;} break;
+
+					}
+
+				}
+
+			}
+
+			public void keyReleased(KeyEvent ke){
+
+				if(main.started){
+
+					switch(ke.getKeyCode()){
+
+						case 87: main.sct.w = false; break;
+						case 65: main.sct.a = false; break;
+						case 83: main.sct.s = false; break;
+						case 68: main.sct.d = false; break;
+
+					}
 
 				}
 
@@ -208,6 +297,8 @@ class MainPane extends JLayeredPane{
 	int currentX;
 	int currentY;
 	JPanel startGamePanel;
+	ServerConnectionThread sct;
+	Boolean started = false;
 
   public MainPane(Wind wind, Dimension size) throws Exception{
 
@@ -310,7 +401,7 @@ class MainPane extends JLayeredPane{
 		startGamePanel.add(reload);
 
 		WaitServerConnecter wsc = new WaitServerConnecter();
-		Boolean connectionRes = wsc.setSocket("localhost",6900);
+		Boolean connectionRes = wsc.setSocket("192.168.4.115",6900);
 
 		if(connectionRes){
 
@@ -379,7 +470,8 @@ class MainPane extends JLayeredPane{
 
 							try{
 								setVisible(false);
-								new ServerConnectionThread(Integer.parseInt(tableData[tmp][1]), jta.getText(), wind);
+								sct = new ServerConnectionThread(Integer.parseInt(tableData[tmp][1]), jta.getText(), wind);
+								started = true;
 							}
 							catch(Exception e){
 								System.out.print(e);
@@ -439,6 +531,10 @@ class ServerConnectionThread extends Thread{
 	int port;
 	String username;
 	Wind wind;
+	Boolean started = true;
+	DataOutputStream dos;
+	Boolean w = false, a = false, s = false, d = false;
+
 
 	public ServerConnectionThread(int port, String username, Wind wind){
 
@@ -453,14 +549,19 @@ class ServerConnectionThread extends Thread{
 
 		try{
 
-			GameServerConnection gsc = new GameServerConnection("localhost", port);
+			GameServerConnection gsc = new GameServerConnection("192.168.4.115", port);
 			gsc.enterServer(username);
 			gsc.downloadMap();
+
+			this.dos = gsc.dos;
+
+			new ResponseThread(this).start();
 
 			wind.screen = new Screen(gsc , wind.size);
 
 			wind.add(wind.screen);
 			wind.setVisible(true);
+
 
 		}
 		catch(Exception e){
@@ -468,6 +569,74 @@ class ServerConnectionThread extends Thread{
 			System.out.print(e);
 
 		}
+
+	}
+
+}
+
+class ResponseThread extends Thread{
+
+	ServerConnectionThread sct;
+	String x = "0";
+	String y = "0";
+
+	public ResponseThread(ServerConnectionThread sct){
+
+		this.sct = sct;
+
+	}
+
+	public void run(){
+
+		try{
+
+			while(sct.started){
+
+				System.out.println(sct.w + " " + sct.a + " " + sct.s + " " + sct.d);
+
+				if(sct.w && sct.s){
+					y = "0";
+				}
+				else{
+					if(sct.w){
+						y = "-1";
+					}
+					else{
+						if(sct.s){
+							y = "1";
+						}
+						else{
+							y = "0";
+						}
+					}
+				}
+
+				if(sct.a && sct.d){
+					x = "0";
+				}
+				else{
+					if(sct.a){
+						x = "-1";
+					}
+					else{
+						if(sct.d){
+							x = "1";
+						}
+						else{
+							x = "0";
+						}
+					}
+				}
+
+				System.out.println(x + "/" + y + "/0");
+
+				sct.dos.writeUTF(x + "/" + y + "/0");
+
+				Thread.sleep(10);
+
+			}
+
+		}catch(Exception e){System.out.print(e);}
 
 	}
 
